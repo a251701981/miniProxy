e941a667472ae9a9221eef0e9006edb036107abe
a251701981 <251701981@qq.com>
2020-09-28 23:24:47 +0800

    Update miniProxy.php

--- miniProxy.php
+++ miniProxy.php
@@ -268,6 +268,7 @@ function makeRequest($url) {
 
   //Make the request.
   $response = curl_exec($ch);
+    var_dump($response);exit;
   $responseInfo = curl_getinfo($ch);
   $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
   curl_close($ch);
