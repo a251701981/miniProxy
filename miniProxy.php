bf23f0e73a123090e2e9357a9d65855ae90b6ba8
a251701981 <251701981@qq.com>
2020-09-28 21:07:49 +0800

    Update miniProxy.php

--- miniProxy.php
+++ miniProxy.php
@@ -240,6 +240,7 @@ function makeRequest($url) {
   curl_setopt($ch, CURLOPT_HEADER, true);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书验证
 
   //Set the request URL.
   curl_setopt($ch, CURLOPT_URL, $url);
